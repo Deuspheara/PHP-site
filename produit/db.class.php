46567b1b818b646f393a11f055c95e3d84369ff9
Deuspheara <quentin.gaillardet@etu.univ-lyon1.fr>
2021-01-22 23:00:46 +0100

    Update db.class.php

--- produit/db.class.php
+++ produit/db.class.php
@@ -1,7 +1,7 @@
 <?php
     // Enter your host name, database username, password, and database name.
     // If you have not set database password on localhost then set empty.
-    $con = mysqli_connect("iutbg-lamp.univ-lyon1.fr","p2001032","12001032","p2001032");
+    $con = mysqli_connect("iutbg-lamp.univ-lyon1.fr","root","","database");
     // Check connection
     if (mysqli_connect_errno()){
         echo "Failed to connect to MySQL: " . mysqli_connect_error();
